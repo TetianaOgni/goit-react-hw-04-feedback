425f616228b598521e39a72fab1a5b80e49e5045
TetianaOgnivenko <tatianaognivenko@gmail.com>
2023-06-25 18:40:26 +0300

    create style for Notification

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import css from './Notification.module.css'
 
 const Notification = ({ message }) => {
-  return <p>{message}</p>;
+  return <p className={css.message}>{message}</p>;
 };
 
 Notification.propTypes = {
